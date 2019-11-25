cdf80f623c403c1711c857ac775bbd263a6a84b8
Nazaroi <nazaroi_1990@yahoo.com>
2019-11-25 12:26:42 +0200

    Added screenshots

--- app/src/main/java/com/sample/musicx/utilities/Constants.kt
+++ app/src/main/java/com/sample/musicx/utilities/Constants.kt
@@ -1,6 +1,6 @@
 package com.sample.musicx.utilities
 
-const val DATABASE_NAME = "musicx_database28"
+const val DATABASE_NAME = "musicx_database29"
 
 const val TRACKS_DATA_FILENAME = "tracks.json"
 const val ALBUMS_DATA_FILENAME = "albums.json"
